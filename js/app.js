c4f186d07937a545b4dd11dc3757cdcc55b0b0a8
Md jewel <jewel3472311@gmail.com>
2024-12-09 18:02:35 +0600

    added the my donation math and style part also

--- js/app.js
+++ js/app.js
@@ -5,18 +5,11 @@ const historySection = document.getElementById("history-section");
 const historyList = document.getElementById("history-list");
 const donationBtn = document.getElementById("donation-btn");
 const historyBtn = document.getElementById("history-btn");
-// const modal = document.getElementById('modal');
-// const closeModal = document.getElementById('close-modal');
-const blogSection = document.getElementById('blog-section');
-const blogBtn = document.getElementById('blog-btn')
-
-// blogBtn.addEventListener("click", () => toggleSections(true));
-
+const blogSection = document.getElementById("blog-section");
+const blogBtn = document.getElementById("blog-btn");
 let balance = parseInt(balanceEl.textContent);
 
-
-
-// Toggle sections
+// Toggle sections history and donation part
 function toggleSections(showDonation) {
   donationSection.style.display = showDonation ? "block" : "none";
   historySection.style.display = showDonation ? "none" : "block";
@@ -24,11 +17,7 @@ function toggleSections(showDonation) {
   donationBtn.classList.toggle("active", showDonation);
   historyBtn.classList.toggle("active", !showDonation);
   blogBtn.classList.toggle("active", !showDonation);
-  // blogBtn.classList.toggle("active", showDonation);
 }
-
-
-
 // Handle donations
 document.querySelectorAll(".donate-btn").forEach((btn) => {
   btn.addEventListener("click", (e) => {
@@ -55,16 +44,13 @@ document.querySelectorAll(".donate-btn").forEach((btn) => {
     // Add to history
     const donationName = card.querySelector(".donation-name").textContent;
     const dateTime = new Date().toLocaleString();
-    // historyList.innerHTML += `<li class='flex'>${dateTime}: Donated ${donationAmount} coins to ${donationName}</li>`;
     historyList.innerHTML += `<div class="border-2 border-e-gray-700 p-5 w-[90%] mx-auto rounded-lg mt-3">
         <h1 class="text-2xl font-bold">${donationAmount} Taka is ${donationName}</h1>
         <p>
           Date : ${dateTime} GMT +0600 (Bangladesh Standard Time)
         </p>
       </div>`;
-
-    // Show modal
-    // modal.classList.add('active');
+    // clear the donation input
     donationInput.value = "";
   });
 });
@@ -73,4 +59,3 @@ document.querySelectorAll(".donate-btn").forEach((btn) => {
 donationBtn.addEventListener("click", () => toggleSections(true));
 historyBtn.addEventListener("click", () => toggleSections(false));
 blogBtn.addEventListener("click", () => toggleSections(false));
-// closeModal.addEventListener('click', () => modal.classList.remove('active'));
